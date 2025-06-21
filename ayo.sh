7ca5126c263d63ec1e9c328a2ee1ced2f48a2e9d
Ayo <ayo@ayco.io>
2025-06-21 22:26:26 +0200

    feat(ayo): pass arguments

--- ayo.sh
+++ ayo.sh
@@ -2,7 +2,7 @@
 
 case $1 in
   j | journal)
-    . ${HOME}/Projects/scripts/journal.sh
+    . ${HOME}/Projects/scripts/journal.sh $2 $3 $4 $5
     ;;
   c | config)
     echo 'Config script in-progress'
