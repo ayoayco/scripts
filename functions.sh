d6afb4e303da8bb22166da0142d8086f2617bb26
Ayo <ayo@ayco.io>
2025-06-23 13:52:05 +0200

    feat: remove success report; we only repoft failure

--- functions.sh
+++ functions.sh
@@ -9,7 +9,6 @@ notesSync() {
     git add .
     git commit -m "[script] update/add entrie/s" --quiet
     git push --quiet
-    echo ">>> Sync success"
   } || {
     # Report; TODO: write log
     echo ">>> Sync failed"
