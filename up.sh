dbfbd03d2c733f881eb41061205ab625b30e7ba7
Ayo <ayo@ayco.io>
2025-06-27 15:00:59 +0200

    feat(up): add script for switching laptop screen resolution

--- up.sh
+++ up.sh
@@ -8,5 +8,17 @@
 
 #command=$1
 
-sudo apt update && sudo apt upgrade -y
-flatpak update -y
+set_display_resolution() {
+  if [ "$1" = "1920" ] || [ "$1" = "1200" ]; then
+    xrandr --output eDP-1 --mode 1920x1200
+  else
+    xrandr --output eDP-1 --mode 1680x1050
+  fi
+}
+
+if [ $1 = "display" ] || [ $1 = "d" ]; then
+  set_display_resolution $2
+else
+  sudo apt update && sudo apt upgrade -y
+  flatpak update -y
+fi
