ed0fcae79b68e484dd92bb8d55215f2b4805f3e8
Ayo <ayo@ayco.io>
2025-07-05 11:53:06 +0200

    feat(display): add big/small aliases

--- display.sh
+++ display.sh
@@ -8,9 +8,9 @@
 
 #command=$1
 
-if [ "$1" = "1920" ] || [ "$1" = "1200" ]; then
+if [ $1 = "big" ] || [ "$1" = "1920" ] || [ "$1" = "1200" ]; then
   xrandr --output eDP-1 --mode 1920x1200
-elif [ "$1" = "1280" ] || [ "$1" = "800" ]; then
+elif [ $1 = "small" ] || [ "$1" = "1280" ] || [ "$1" = "800" ]; then
   xrandr --output eDP-1 --mode 1280x800
 else
   xrandr --output eDP-1 --mode 1680x1050
