346a7271618a32da0b359097e3a76da832738bc5
Ayo <ayo@ayco.io>
2025-08-22 00:26:13 +0200

    feat: pass string to ai

--- ai.sh
+++ ai.sh
@@ -1,6 +1,9 @@
 sudo systemctl start ollama
-if [ $1 = "llama" ]; then
-  ollama run llama3:8b
+sleep 1
+
+
+if ! [ "$2" = "" ]; then
+  ollama run gpt-oss:20b "$2"
 else
   ollama run gpt-oss:20b
 fi
--- ayo.sh
+++ ayo.sh
@@ -10,23 +10,23 @@ case $1 in
   ## SHORTCUTS
 
   ja) # journal append
-    . ${scripts_dir}/journal.sh append $2 $3 $4 $5 $6 $7 $8 $9
+    . ${scripts_dir}/journal.sh append "$@"
     ;;
   jt) # journal using typora
-    . ${scripts_dir}/journal.sh -t $2 $3 $4 $5 $6 $7 $8 $9
+    . ${scripts_dir}/journal.sh -t "$@"
     ;;
 
   tl) # Tasks list
-  . ${scripts_dir}/tasks.sh list $2 $3 $4 $5 $6 $7 $8 $9
+  . ${scripts_dir}/tasks.sh list "$@"
   ;;
   to) # Tasks open
-  . ${scripts_dir}/tasks.sh open $2 $3 $4 $5 $6 $7 $8 $9
+  . ${scripts_dir}/tasks.sh open "$@"
   ;;
   tr) # Tasks remove
-  . ${scripts_dir}/tasks.sh remove $2 $3 $4 $5 $6 $7 $8 $9
+  . ${scripts_dir}/tasks.sh remove "$@"
   ;;
   td) # Tasks mark as done
-  . ${scripts_dir}/tasks.sh done $2 $3 $4 $5 $6 $7 $8 $9
+  . ${scripts_dir}/tasks.sh done "$@"
   ;;
 
   nd) # Notes diff
@@ -36,32 +36,32 @@ case $1 in
     . ${scripts_dir}/notes.sh sync
     ;;
   nl) # Notes list
-    . ${scripts_dir}/notes.sh list $2 $3 $4 $5 $6 $7 $8 $9
+    . ${scripts_dir}/notes.sh list "$@"
     ;;
   no) # Notes open
-    . ${scripts_dir}/notes.sh open $2 $3 $4 $5 $6 $7 $8 $9
+    . ${scripts_dir}/notes.sh open "$@"
     ;;
   nr) # Notes remove
-    . ${scripts_dir}/notes.sh remove $2 $3 $4 $5 $6 $7 $8 $9
+    . ${scripts_dir}/notes.sh remove "$@"
     ;;
   na) # Notes archive
-    . ${scripts_dir}/notes.sh archive $2 $3 $4 $5 $6 $7 $8 $9
+    . ${scripts_dir}/notes.sh archive "$@"
     ;;
   nc) # Notes copy
-    . ${scripts_dir}/notes.sh copy $2 $3 $4 $5 $6 $7 $8 $9
+    . ${scripts_dir}/notes.sh copy "$@"
     ;;
   nt) # Notes using typora
-    . ${scripts_dir}/notes.sh -t $2 $3 $4 $5 $6 $7 $8 $9
+    . ${scripts_dir}/notes.sh -t "$@"
     ;;
 
   gd) # git diff
-    . ${scripts_dir}/git.sh diff $2 $3 $4 $5 $6 $7 $8 $9
+    . ${scripts_dir}/git.sh diff "$@"
     ;;
   gs) # git status
-    . ${scripts_dir}/git.sh stat $2 $3 $4 $5 $6 $7 $8 $9
+    . ${scripts_dir}/git.sh stat "$@"
     ;;
   gp) # git push
-    . ${scripts_dir}/git.sh push $2 $3 $4 $5 $6 $7 $8 $9
+    . ${scripts_dir}/git.sh push "$@"
     ;;
 
   mt) # mail task
@@ -71,28 +71,28 @@ case $1 in
   ## SCRIPTS
 
   ai)
-    . ${scripts_dir}/ai.sh $2 $3 $4 $5 $6 $7 $8 $9
+    . ${scripts_dir}/ai.sh "$@"
     ;;
   m | mail)
-    . ${scripts_dir}/mail.sh $2 $3 $4 $5 $6 $7 $8 $9
+    . ${scripts_dir}/mail.sh "$@"
     ;;
   d | display)
-    . ${scripts_dir}/display.sh $2 $3 $4 $5 $6 $7 $8 $9
+    . ${scripts_dir}/display.sh "$@"
     ;;
   u | up)
-    . ${scripts_dir}/up.sh $2 $3 $4 $5 $6 $7 $8 $9
+    . ${scripts_dir}/up.sh "$@"
     ;;
   g | git)
-    . ${scripts_dir}/git.sh $2 $3 $4 $5 $6 $7 $8 $9
+    . ${scripts_dir}/git.sh "$@"
     ;;
   n | notes)
-    . ${scripts_dir}/notes.sh $2 $3 $4 $5 $6 $7 $8 $9
+    . ${scripts_dir}/notes.sh "$@"
     ;;
   t | tasks)
-    . ${scripts_dir}/tasks.sh $2 $3 $4 $5 $6 $7 $8 $9
+    . ${scripts_dir}/tasks.sh "$@"
     ;;
   j | journal)
-    . ${scripts_dir}/journal.sh $2 $3 $4 $5 $6 $7 $8 $9
+    . ${scripts_dir}/journal.sh "$@"
     ;;
   c | config)
     echo 'Config script in-progress'
