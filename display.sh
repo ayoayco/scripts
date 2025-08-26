ba2a0da0af233b0441b402888915f65428fb3d77
Ayo <ayo@ayco.io>
2025-08-26 10:12:02 +0200

    feat(display): dual monitor modes (right, left, center)

--- display.sh
+++ display.sh
@@ -25,13 +25,29 @@ function main() {
       echo "Listing all monitors"
       xrandr --listmonitors
       ;;
-    "dual")
-      echo "Setting dual display mode"
+    "dual"|"right")
+      echo "Setting dual display right"
       xrandr --output "$extern" --primary --auto --left-of "$intern" --output "$intern" --mode 1680x1050
+      ;;
+    "left")
+      echo "Setting dual display left"
+      xrandr --output "$extern" --primary --auto --right-of "$intern" --output "$intern" --mode 1680x1050
+      ;;
+    "center")
+      echo "Setting dual display center"
+      xrandr \
+        --output "$extern" --auto --above "$intern" \
+        --output "$intern" --primary --auto
+      ;;
+    "ultra")
+      echo "Setting single display mode (ultrawide)"
+      xrandr --output "$intern" --off \
+        --output "$extern" --auto
       ;;
     "solo")
       echo "Setting single display mode (internal only)"
-      xrandr --output "$extern" --off --output "$intern" --auto
+      xrandr --output "$extern" --off \
+        --output "$intern" --auto
       ;;
     *)
       echo "Setting display to preferred size (1680x1050)"
